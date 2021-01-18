209cd8b409349c90a0ec5eff8a7a3a92efa820cc
Wenny Hidayat <wenny.hidayat@digio.com.au>
2021-01-18 18:13:24 +1100

    Sample simple navigation

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
 
+import './category_meals_screen.dart';
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
+  // Sample simple navigation (push-pop)
+  void selectCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+      return CategoryMealsScreen();
+    }));
+  }
   CategoryItem(this.title, this.color);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(
-          15), // optimised so EdgeInsets doesn't get rerendered
-      child: Text(title, style: Theme.of(context).textTheme.headline6  ,),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-        colors: [color.withOpacity(0.7), color],
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(
+            15), // optimised so EdgeInsets doesn't get rerendered
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [color.withOpacity(0.7), color],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(15)),
       ),
-      borderRadius: BorderRadius.circular(15)),
     );
   }
 }
---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'The Recipes',
+        ),
+      ),
+      body: Center(
+        child: Text(
+          'Recipes for this category',
+        ),
+      ),
+    );
+  }
+}
